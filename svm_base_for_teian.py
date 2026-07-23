d81a1975210dcbf820cc2ec1eb05c614e5c55855
rukarp <pgeu1dd7@s.okayama-u.ac.jp>
2026-07-23 11:50:50 +0900

    Update 2026-07-23 11:50:50

--- svm_base_for_teian.py
+++ svm_base_for_teian.py
@@ -32,6 +32,182 @@ SAVE_DIR = "fig/svm"
 
 # 提案手法用のクラス
 class BaseSVM_for_Teian(MySVM):   
+
+    def fit(self, X, y, A, L):
+            """
+            SVMモデルをデータに適合させる関数(提案手法)
+            パラメータ:
+            X (array-like): トレーニングデータの特徴量
+            y (array-like): トレーニングデータのラベル
+            A: 隣接行列(自分の拠点の列だけ)
+            L: グラフ直径
+            """
+            # 隣接行列
+            self.A = A
+    
+            # グラフ直径
+            self.L = L
+    
+            # 目的関数の値の確認する回数
+            self.history_check = size
+            
+            # オリジナルデータのラベルの最小値と最大値を取得
+            self.min_val = np.min(y)
+            self.max_val = np.max(y)
+            
+            # 初期状態のデータを保存
+            self.X_ori = X
+            self.y_ori = y
+            
+            # データの個数を保存する配列
+            self.num_samples = np.empty((0, 2), dtype=int)
+            
+            # データの個数を保存する配列(データ消去をしない場合の通算個数)
+            self.num_samples_total = np.empty(0, dtype=int)
+            
+            # 目的関数の末尾の値を保存する配列
+            self.objective_last_value = np.zeros(1, dtype=float)
+    
+            self.is_converged = False
+            
+            
+            #------------ alphaの学習(個別に学習してサポートベクターをもらう方法) -------------#
+            
+            # データの個数を共有し，箱を用意
+            num_samples_ori_list = comm.allgather(len(self.X_ori))
+            start_ind = sum(num_samples_ori_list[:rank])
+            end_ind = start_ind + num_samples_ori_list[rank]
+            
+            self.my_ind = np.arange(start_ind, end_ind)
+            
+            self.num_samples_all = sum(num_samples_ori_list)
+            
+            #print(rank, start_ind, end_ind, num_samples_ori_list, flush=True)
+            self.X = np.full((self.num_samples_all, self.X_ori.shape[1]), np.nan, dtype=float)
+            self.y = np.zeros(self.num_samples_all, dtype=int)
+            self.alphas = np.zeros(self.num_samples_all, dtype=float)
+            
+            self.X[self.my_ind] = self.X_ori
+            self.y[self.my_ind] = self.y_ori
+            #print(rank, self.X_all.shape, self.y_all.shape, self.y_all, flush=True)
+            
+            self.K = np.full((self.num_samples_all, self.num_samples_all), np.nan, dtype=float)
+            
+            for roop in range(self.max_roop): # ループ回数は仮置き
+                
+                comm.Barrier()
+                if rank == 0:  
+                    print(f"roop {roop}", flush=True)
+                comm.Barrier()
+    
+                # 学習データの通算個数を格納
+                self.active_all_ind = np.where(~np.isnan(self.X[:, 0]))[0]
+                self.num_samples_total = np.append(self.num_samples_total, len(self.active_all_ind))
+                
+                # 学習に使用するインデックスを獲得し，学習データを作成
+                active_ind = np.where(self.y != 0)[0]
+                active_X = self.X[active_ind]
+                active_y = self.y[active_ind]
+                self.num_samples = np.vstack((self.num_samples, np.array([roop, len(active_ind)]))) 
+    
+                # 個別に学習して alphas を得る
+                self.alphas[:] = 0
+                self.alphas[active_ind], self.K[np.ix_(active_ind, active_ind)], self.objective_value = self.fit_independent(active_X, active_y)
+                self.ind_sv, self.ind_inner = self._get_SV_ind(self.alphas)
+    
+                # 目的関数の末尾の値を保存
+                self.objective_last_value = np.append(self.objective_last_value, self.objective_value[-1])
+    
+                
+                
+                # 自分の拠点のデータでない且つalphasの値が0になったものを非アクティブ化する
+                ind_zero_alphas = np.where(self.alphas <= self.ME)[0]                        
+                self.y[ind_zero_alphas] = 0
+                self.y[self.my_ind] = self.y_ori
+                            
+                # データ削除後の学習データの個数
+                self.num_samples = np.vstack((self.num_samples, np.array([roop, len(np.where(self.y != 0)[0])])))             
+                
+                
+                
+                
+                
+    
+                # 目的関数の値がsize回変化なかったか確認
+                history_obj = self.objective_last_value[-self.L:]
+                my_converged = all(obj == history_obj[0] for obj in history_obj)
+                all_converged = comm.allgather(my_converged)
+                self.is_converged = all(all_converged[i] == True for i in range(size))
+                #print(rank, self.is_converged, history_obj, flush=True)
+    
+    
+                comm.Barrier()  
+                if rank == 0:  
+                    print("", flush=True)
+                comm.Barrier()
+    
+                # 終了判定
+                if (self.is_converged == True) or (roop == self.max_roop - 1):
+                    
+                    # 重みwを計算
+                    if self.kernel == 'linear':
+                        self.w = self._calculate_w(self.X, self.y, self.alphas, self.ind_sv, self.ind_inner)
+                    else:
+                        self.w = None
+    
+                    # バイアスbを計算
+                    self.b = self._calculate_b(self.y, self.alphas, self.K, self.ind_sv, self.ind_inner)
+                    
+                    # 識別関数を計算
+                    self.f = self.make_decision_func(self.X, self.y, self.alphas, self.w, self.b, self.ind_sv, self.ind_inner)   
+                    
+                    self.roop = roop
+    
+                    break  
+    
+    
+                
+    
+                
+                
+                # alphasが0より大きくなるインデックスに対応するX, yを獲得
+                ind_active_alphas = np.concatenate((self.ind_sv, self.ind_inner))
+                X_sv_inner = self.X[ind_active_alphas]
+                y_sv_inner = self.y[ind_active_alphas]
+                                
+                            
+                # データ一組（特徴量1行 + ラベル1つ + インデント番号1つ）のバイトサイズを計算
+                data_unit_size = (X_sv_inner.itemsize * X_sv_inner.shape[1]) + y_sv_inner.itemsize + ind_active_alphas.itemsize
+                # チャンクサイズを計算（バッファサイズ内に収まるデータ一組の数）
+                chunk_size = buffer_size // data_unit_size - 1
+                # サポートベクターのチャンク数を計算
+                num_chunks = (len(X_sv_inner) + chunk_size - 1) // chunk_size
+    
+                # データ送信・受信の実装
+                for neighbor in range(size):
+                    if self.A[neighbor] == 1:
+                                
+                        # 送信するチャンク数を相手に送信
+                        req_send_chunks = comm.isend(num_chunks, dest=neighbor)
+                        req_recv_chunks = comm.irecv(source=neighbor)
+                        recv_num_chunks = req_recv_chunks.wait()  # 受信するチャンク数
+                        req_send_chunks.wait()
+    
+                        # 各チャンクを送信
+                        for i in range(num_chunks):
+                            start = i * chunk_size
+                            end = min((i + 1) * chunk_size, len(X_sv_inner))
+                
+                            req_send = comm.isend((X_sv_inner[start:end], y_sv_inner[start:end], ind_active_alphas[start:end]), dest=neighbor)
+                            req_send.wait()
+    
+                        # 各チャンクを受信し，データを追加
+                        for i in range(recv_num_chunks):
+                            req_recv = comm.irecv(source=neighbor)
+                            recv_X, recv_y, recv_ind = req_recv.wait()
+    
+                            self.X[recv_ind] = recv_X
+                            self.y[recv_ind] = recv_y
     
     
     def fit_independent(self, X, y):
